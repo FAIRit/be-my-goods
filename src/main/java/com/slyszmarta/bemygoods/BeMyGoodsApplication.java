f513281e4d8932dd465e91ac3b2179f62cae7de5
Marta <martaslysz93@gmail.com>
2020-03-02 15:50:44 +0100

    Basic project configuration from Springboot starter added.

---
+++ src/main/java/com/slyszmarta/bemygoods/BeMyGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.slyszmarta.bemygoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMyGoodsApplication.class, args);
+    }
+
+}
